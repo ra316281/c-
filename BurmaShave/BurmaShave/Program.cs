289d6cb6a79de10cb09aa8031a1c13585138291f
ra316281 <35504903+ra316281@users.noreply.github.com>
2018-01-19 13:56:11 -0600

    BrumaShave

---
+++ BurmaShave/BurmaShave/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Angels");
+            Console.WriteLine("Who guard you");
+            Console.WriteLine("When you drive");
+            Console.WriteLine("Usually");
+            Console.WriteLine("Reture at 65");
+        }
+    }
+}
