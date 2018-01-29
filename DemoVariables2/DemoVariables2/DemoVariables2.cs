f97987af8751988dc6719dbce55f76e2e7bd39b1
ra316281 <35504903+ra316281@users.noreply.github.com>
2018-01-29 14:54:43 -0600

    age

--- DemoVariables2/DemoVariables2/DemoVariables2.cs
+++ DemoVariables2/DemoVariables2/DemoVariables2.cs
@@ -13,15 +13,15 @@ namespace ConsoleApplication1
             int value1 = 43, value2 = 10, sum, diff, product, quotient, remainder;
 
             sum = value1 + value2;
-            //diff = value1 - value2;
-            //product = value1 * value2;
-            //quotient = value1 / value2;
+            diff = value1 - value2;
+            product = value1 * value2;
+            quotient = value1 / value2;
             remainder = value1 % value2;
-            Console.WriteLine("The sum of {0} and {1] is {2}",value1, value2, sum);
+            //Console.WriteLine("The sum of {0} and {1] is {2}",value1, value2, sum);
             //Console.WriteLine("The difference between {0} and {1}" + "is {2}", value1, value2, diff);
-            //Console.WriteLine("The product of {0} and {1} is {2}",value1, value2, product);
-            //Console.WriteLine("{0} divided by {1} is {2}", value1, value2, quotient);
-            //Console.WriteLine("and the remainder is {0]", remainder);
+            Console.WriteLine("The product of {0} and {1} is {2}",value1, value2, product);
+            Console.WriteLine("{0} divided by {1} is {2}", value1, value2, quotient);
+            Console.WriteLine("and the remainder is {0]", remainder);
 
         }
     }
