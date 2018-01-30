f089f2b4ca3f6bec8e0e1ea8fe9f4dba4bfa02cb
ra316281 <35504903+ra316281@users.noreply.github.com>
2018-01-30 14:33:54 -0600

    update

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lblHello = new System.Windows.Forms.Label();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(27, 63);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click here";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // lblHello
+            // 
+            this.lblHello.AutoSize = true;
+            this.lblHello.Location = new System.Drawing.Point(12, 9);
+            this.lblHello.Name = "lblHello";
+            this.lblHello.Size = new System.Drawing.Size(99, 13);
+            this.lblHello.TabIndex = 1;
+            this.lblHello.Text = "Hello, Visual World!";
+            this.lblHello.Visible = false;
+            // 
+            // btnLast
+            // 
+            this.btnLast.Enabled = false;
+            this.btnLast.Location = new System.Drawing.Point(27, 92);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 3;
+            this.btnLast.Text = "Click me last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(140, 147);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.lblHello);
+            this.Controls.Add(this.btnClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lblHello;
+        private System.Windows.Forms.Button btnLast;
+    }
+}
+
