3324208fc40a4463b538c147de1a1ea6d3ff890e
ra316281 <35504903+ra316281@users.noreply.github.com>
2018-02-01 14:41:27 -0600

    upload

---
+++ CompareThreeNumbers/CompareThreeNumbers/CompareThreeNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class CompareThreeNumbers
+    {
+        static void Main(string[] args)
+        {
+            string numberString;
+            int num1, num2, num3;
+            Console.WriteLine("Enter an iteger ");
+            numberString = Console.ReadLine();
+            num1 = Convert.ToInt32(numberString);
+            Console.WriteLine("Enter an integer ");
+            numberString = Console.ReadLine();
+            num2 = Convert.ToInt32(numberString);
+            Console.WriteLine("Enter an integer");
+            numberString = Console.ReadLine();
+            num3 = Convert.ToInt32(numberString);
+            if (num1 == num2)
+                if (num1 == num3)
+                    Console.WriteLine("All three numbers are equal");
+                else
+                    Console.WriteLine("First two are equal");
+            else
+                if (num1 == num3)
+                    Console.WriteLine("Last two are equal");
+                else
+                    if (num2 == num3)
+                        Console.WriteLine("Last two are equal");
+                    else
+                        Console.WriteLine("No two numbers are equal");
+               
+
+
+        }
+    }
+}
