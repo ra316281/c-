fe0dff9ffc6997512f49926fb18de80641235203
ra316281 <35504903+ra316281@users.noreply.github.com>
2018-02-01 14:40:55 -0600

    upload

---
+++ GreenvilleRevenue/GreenvilleRevenue/GreenvileRevenue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class GreenvileRevenue
+    {
+        static void Main(string[] args)
+        {
+            int value1 = 25, value2,
+                product;
+            product = value1 * value2;
+
+
+        }
+    }
+}
---
+++ highNum Lownum/highNum Lownum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace highNum_Lownum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int theNumber = 10;
+            string usernum;
+            int userg;
+            Console.WriteLine("Choose a number between 1 amd 20");
+            usernum = Console.ReadLine();
+            userg = Convert.ToInt32(usernum);
+            if (userg > theNumber)
+            {
+                Console.WriteLine("The number you selected was too high");
+            }
+            else if (userg < theNumber)
+            {
+                Console.WriteLine("The number you selected was too low");
+            }
+            else if (userg == theNumber)
+                Console.WriteLine("The number you selected is correct");
+
+            
+
+
+
+            //Choose a number between 1 and 20
+            //The number you selected was too high
+            //The number you selected was too low
+            //The number you selected is correct
+
+
+        }
+    }
+}
