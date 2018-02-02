467bbc4de95557a1c1f8bca5ee61f2a9984d219c
ra316281 <35504903+ra316281@users.noreply.github.com>
2018-02-02 10:07:26 -0600

    update

---
+++ DemoORAndAND/DemoORAndAND/DemoORAndAND.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class DemoORAndAND
+    {
+        static void Main(string[] args)
+        {
+            const int ZONE1 = 1, ZONE2 = 2;
+            const int LOW_QUANTITY = 10;
+            string inputString;
+            int quantity;
+            int deliveryZone;
+            Console.WriteLine("Delivery is free for zone {0} or {1", ZONE1, ZONE2);
+            Console.WriteLine("WHEN THE NUMBER OF BOXES IS LESS THAN {0}", LOW_QUANTITY);
+            Console.WriteLine("Enter delivery zone");
+            inputString = Console.ReadLine();
+            deliveryZone = Convert.ToInt32(inputString);
+            Console.WriteLine("Enter the number of boxes in the shipment");
+            inputString = Console.ReadLine();
+            quantity = Convert.ToInt32(inputString);
+            if((deliveryZone == ZONE1 || deliveryZone == ZONE2) && quantity < LOW_QUANTITY)
+                Console.WriteLine("Delivery is free");
+            else
+                Console.WriteLine("A delivery charge applies");
+        }
+    }
+}
