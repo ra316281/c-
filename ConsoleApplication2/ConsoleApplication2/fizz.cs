6cd79c04f3cf58914b937b5029a593d6225f9388
ra316281 <35504903+ra316281@users.noreply.github.com>
2018-02-21 13:59:05 -0600

    update

---
+++ ConsoleApplication2/ConsoleApplication2/fizz.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class fizz
+    {
+        static void Main(string[] args)
+        {
+            string numberguessed;
+            string stringStop;
+            string num3 = "Fizz";
+            string num5 = "Buzz";
+            string num7 = "Woof";
+            string num15 = "FizzBuzz";
+            const int Num3 = 3, Num5 = 5, Num7 = 7;
+            int number;
+           
+           
+
+
+      
+
+            do 
+            {
+                 Console.WriteLine("Enter an integer!");
+                numberguessed = Console.ReadLine();
+                number = Convert.ToInt32(numberguessed);
+
+                if (number/3 == 0 || number%3 == 0)
+                {
+                    Console.WriteLine("{0}", num3);
+                }
+                
+                if (number == Num5)
+                {
+                    Console.WriteLine("{0}", num5);
+                }
+
+                if (number == Num7)
+                {
+                    Console.WriteLine("{0}",num7);
+                }
+
+                if (number / Num3 == Num3)
+                {
+                    Console.WriteLine("{0}", num3);
+                }
+
+                if (number == Num6)
+                {
+                    Console.WriteLine("{0}", num3);
+                }
+
+                if (number == Num10)
+                {
+                    Console.WriteLine("{0}", num5);
+                }
+
+                if (number == Num12)
+                {
+                    Console.WriteLine("{0}", num3);
+                }
+
+                if (number == Num14)
+                {
+                    Console.WriteLine("{0}", num7);
+                }
+
+                if (number == Num15)
+                {
+                    Console.WriteLine("{0}", num15);
+                }
+                Console.WriteLine("Enter an integer!");
+                stringStop = Console.ReadLine();
+               
+            }while( stringStop != "stop" );
+        }
+    }
+}
