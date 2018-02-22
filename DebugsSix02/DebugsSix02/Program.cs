a7d6000075df8c2aa1a8a97fd946523ec84d5b92
ra316281 <35504903+ra316281@users.noreply.github.com>
2018-02-22 14:58:15 -0600

    update

---
+++ DebugsSix02/DebugsSix02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+// Program averages four numbers
+using System.Console;
+class DebugSix02
+{
+   static void Main()
+   {
+      int[] numbers = {12, 15, 22, 88};
+      int x;
+      double average;
+      double total = 0;
+       {
+           Console.WriteLine("\nThe numbers are...");
+       }
+      
+      for(x = 0; x < numbers.Length; ++x)
+         Console.WriteLine("{0, 6}", numbers[x]);
+         Console.WriteLine();   
+   
+      for(x = 0; x < numbers.Length; ++x)
+      {
+         total = numbers[x];
+      }   
+      average = total / numbers.Length;
+      Console.WriteLine("The average is {0}");
+   }
+}
