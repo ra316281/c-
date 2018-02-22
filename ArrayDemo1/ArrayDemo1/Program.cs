0088bb309066ea890a822a64877b224f5147b411
ra316281 <35504903+ra316281@users.noreply.github.com>
2018-02-22 14:57:22 -0600

    update

---
+++ ArrayDemo1/ArrayDemo1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] payRate = { 6.00, 7.35, 8.12, 12.45 };
+            for(int x = 3; x < payRate.Length; --x)
+            {
+                Console.WriteLine("pay rate {0} is {1}", x, payRate[x].ToString("C"));
+            }
+        }
+    }
+}
