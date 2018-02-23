c3b4ce96a1b39101d9df51cd4c71e57396ee7b4a
ra316281 <35504903+ra316281@users.noreply.github.com>
2018-02-23 10:08:11 -0600

    update

---
+++ DebugSix03/DebugSix03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugSix03
+// Finds whether a book dealer carries a requested book
+using System;
+using System.Console;
+class DebugSix03
+{
+   static void Main()
+   {
+      String[] books = {"Catch-22", "Harry Potter", "Programming Using C#", "Rich Dad, Poor Dad","The Deep", "Wizard of Oz"};
+      int x;
+      string entryString;
+      
+
+      Console.WriteLine("What book are you looking for? ");
+      entryString = Console.ReadLine();
+      Array.BinarySearch(books);
+      if(x < 0)
+         Console.WriteLine("{0} not found", entryString);
+      else
+         Console.WriteLine("Yes, we carry {0}", entryString);   
+   }
+}
